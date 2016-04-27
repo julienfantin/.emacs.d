e8b0821b6af42479fa86af0917e9da958b959461
Julien Fantin <julienfantin@gmail.com>
2016-04-26 22:08:59 -0400

    Init cleanup

--- init.el
+++ init.el
@@ -1,31 +1,41 @@
-;;; init.el --- Config bootstrap
+;;; init.el --- Config init                          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Julien Fantin
+
+;; Author: Julien Fantin <julienfantin@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
 ;;
+
 ;;; Code:
+(require 'cl-lib)
+(require 'cl-macs)
+(require 'package)
 
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;; (package-initialize)
-
-
 ;; * Config options
 
 (defvar config-completion-system 'ivy)
 
 
 ;; * Bootstrap
-
-(require 'cl-lib)
-(require 'cl-macs)
-(require 'package)
-
 ;; ** Path
 
-;; Make sure the 'user-emacs-directory' is properly set, a lot of our path
-;; definitions rely on this
 (setq user-emacs-directory (expand-file-name "~/.emacs.d/"))
 
 (defun -reload-init ()
