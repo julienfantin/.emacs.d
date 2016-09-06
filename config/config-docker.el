8e9106a4c80802dc99266b85e24881045c153046
Julien Fantin <julienfantin@gmail.com>
2016-09-06 00:21:51 -0400

    Add docker config

---
+++ config/config-docker.el
@@ -0,0 +1,38 @@
+;;; config-docker.el --- Docker config               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Julien Fantin
+
+;; Author: Julien Fantin <julienfantin@gmail.com>
+;; Keywords: processes, tools, unix, abbrev
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(require 'use-config)
+(require 'config-yaml)                  ; for docker-compose
+
+(use-package dockerfile-mode
+  :ensure t
+  :defer t)
+
+(use-package docker
+  :ensure t
+  :defer t)
+
+(provide 'config-docker)
+;;; config-docker.el ends here
