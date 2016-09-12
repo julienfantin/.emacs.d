06e9dd344b3447816f693c7a122b26e74022e69e
Julien Fantin <julienfantin@gmail.com>
2016-09-11 21:44:36 -0400

    Add counsel-flycheck

--- config/config-parsers.el
+++ config/config-parsers.el
@@ -40,7 +40,14 @@
   :functions
   (flycheck-buffer
    flycheck-list-errors
-   flycheck-display-error-messages)
+   flycheck-display-error-messages
+   flycheck-error-list-mode
+   flycheck-error-pos
+   flycheck-error-list-set-source
+   flycheck-error-list-reset-filter
+   config-parsers-flycheck-select-window
+   config-parsers-flycheck-turn-messages-on
+   config-parsers-flycheck-turn-messages-off)
   :config
   (progn
     ;; Custom
@@ -50,8 +57,7 @@
       (select-window (get-buffer-window flycheck-error-list-buffer)))
     (advice-add #'flycheck-list-errors :after #'config-parsers-flycheck-select-window)
     ;; Conditionally disabled error messages
-    (defvar config-parsers-flycheck-display-errors-function
-      #'flycheck-display-error-messages)
+    (defvar config-parsers-flycheck-display-errors-function #'flycheck-display-error-messages)
     (defun config-parsers-flycheck-turn-messages-off (&optional _)
       (setq flycheck-display-errors-function nil))
     (defun config-parsers-flycheck-turn-messages-on (&optional _)
@@ -69,7 +75,32 @@
     (after 'company
       (add-hook 'company-completion-started-hook #'config-parsers-flycheck-turn-messages-off)
       (add-hook 'company-completion-finished-hook #'config-parsers-flycheck-turn-messages-on)
-      (add-hook 'company-completion-cancelled-hook #'config-parsers-flycheck-turn-messages-on))))
+      (add-hook 'company-completion-cancelled-hook #'config-parsers-flycheck-turn-messages-on))
+    (after 'counsel
+      ;; https://github.com/nathankot/dotemacs/blob/ef76773c69cac36c04935edcfa631052bd2c679d/init.el#L566
+      (defvar counsel-flycheck-history nil
+        "History for `counsel-flycheck'")
+      (defun counsel-flycheck ()
+        (interactive)
+        (if (not (bound-and-true-p flycheck-mode))
+            (message "Flycheck mode is not available or enabled")
+          (ivy-read "Flycheck: "
+                    (let ((source-buffer (current-buffer)))
+                      (with-current-buffer (or (get-buffer flycheck-error-list-buffer)
+                                               (progn
+                                                 (with-current-buffer
+                                                     (get-buffer-create flycheck-error-list-buffer)
+                                                   (flycheck-error-list-mode)
+                                                   (current-buffer))))
+                        (flycheck-error-list-set-source source-buffer)
+                        (flycheck-error-list-reset-filter)
+                        (revert-buffer t t t)
+                        (split-string (buffer-string) "\n" t " *")))
+                    :action (lambda (s &rest _)
+                              (-when-let* ( (error (get-text-property 0 'tabulated-list-id s))
+                                            (pos (flycheck-error-pos error)) )
+                                (goto-char (flycheck-error-pos error))))
+                    :history 'counsel-flycheck-history))))))
 
 
 ;; * Semantic
@@ -100,7 +131,8 @@
 (use-package semantic/db
   :defer t
   :defines
-  (semanticdb-database-list)
+  (semanticdb-database-list
+   semanticdb-file-table-object)
   :config
   ;; Redefined as a fix for:
   ;; http://debbugs.gnu.org/cgi/bugreport.cgi?bug=22287
