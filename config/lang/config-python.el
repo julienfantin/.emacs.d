626fc1567862b2dee6a5e6d0923da30a861f7acd
Julien Fantin <jfantin@lyft.com>
2018-07-29 19:08:37 -0700

    Remove lsp config

--- config/lang/config-python.el
+++ config/lang/config-python.el
@@ -30,48 +30,9 @@
   (python-shell-interpreter "python3"))
 
 (use-package eglot
-  :ensure-system-package (pyls . "pip install 'python-language-server[all]' pyls-isort")
-  :hook (python-mode . eglot-ensure))
-
-(use-package lsp-mode
   :ensure t
   :ensure-system-package (pyls . "pip install 'python-language-server[all]' pyls-isort")
-  :hook (python-mode . lsp-python-enable)
-  ;; make sure we have lsp-imenu everywhere we have LSP
-
-  ;; get lsp-python-enable defined
-  ;; NB: use either projectile-project-root or ffip-get-project-root-directory
-  ;;     or any other function that can be used to find the root directory of a project
-  :config
-  (lsp-define-stdio-client lsp-python "python" #'projectile-project-root '("pyls"))
-
-  ;; NB: only required if you prefer flake8 instead of the default
-  ;; send pyls config via lsp-after-initialize-hook -- harmless for
-  ;; other servers due to pyls key, but would prefer only sending this
-  ;; when pyls gets initialised (:initialize function in
-  ;; lsp-define-stdio-client is invoked too early (before server
-  ;; start)) -- cpbotha
-  ;; (defun lsp-set-cfg ()
-  ;;   (let ((lsp-cfg `(:pyls (:configurationSources ("flake8")))))
-  ;;     ;; TODO: check lsp--cur-workspace here to decide per server / project
-  ;;     (lsp--set-configuration lsp-cfg)))
-
-  ;; (add-hook 'lsp-after-initialize-hook 'lsp-set-cfg)
-  )
-
-(use-package lsp-imenu
-  :after lsp-mode
-  :hook (lsp-after-open . lsp-enable-imenu))
-
-(use-package lsp-ui
-  :ensure t
-  :after lsp-mode
-  :hook (lsp-mode . lsp-ui-mode)
-  :custom (lsp-ui-sideline-ignore-duplicate t))
-
-(use-package company-lsp
-  :after lsp-mode
-  :init (push 'company-lsp company-backends))
+  :hook (python-mode . eglot))
 
 (provide 'config-python)
 ;;; config-python.el ends here
