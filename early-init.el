3d4324ec144689221e811c554e259164ec4cf15f
Julien Fantin <julienfantin@gmail.com>
2020-06-13 00:31:11 -0400

    Early init tweaks

--- early-init.el
+++ early-init.el
@@ -71,5 +71,11 @@
 ;; Straight setup recommendation
 (setq package-enable-at-startup nil)
 
+;; Allow loading from the package cache.
+(setq package-quickstart t)
+
+;; Do not resize the frame at this early stage.
+(setq frame-inhibit-implied-resize t)
+
 (provide 'early-init)
 ;;; early-init.el ends here
