45e40620e8bbb4524e945904cf6ce0c8d5c0f091
Julien Fantin <julienfantin@gmail.com>
2020-06-20 14:59:30 -0400

    vterm

--- init.el
+++ init.el
@@ -100,6 +100,7 @@
 (use-package config-search :load-path "./config")
 (use-package config-sexp :load-path "./config")
 (use-package config-shell :load-path "./config")
+(use-package config-term :load-path "./config")
 (use-package config-windows :load-path "./config")
 
 ;; ** Langs
